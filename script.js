a2d23e1b250ce71c1865130ad4d11b2a0b313074
Patrick Dunn <pmdunn78@gmail.com>
2021-03-07 11:12:59 -0600

    displaying time as 12 hour

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ $(document).ready(() => {
     var pageTime = $('#page-time');
     
     const setDateTime = () => {
-        let rightNow = new Date().toString();
-        let date = rightNow.substring(0, 15);
-        let time = rightNow.substring(16, 24);
+        let rightNow = new Date();
+        let date = rightNow.toString().substring(0, 15);
+        let time = rightNow.toLocaleString(
+            'en-US', 
+            { 
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+                hour12: true
+            });
 
         pageDate.text(date);
         pageTime.text(time);
